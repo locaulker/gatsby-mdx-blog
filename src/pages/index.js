de2d0085b9dd09bf0d9cee9c2c6abfc9293b3a7e
Lawrence <Caulker>
2020-06-01 18:04:32 -0400

    introduced styling

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,5 +1,10 @@
 import React from "react"
+import Layout from "../components/Layout"
+import { Link } from "gatsby"
 
-export default function Home() {
-  return <div>Hello world!</div>
-}
+export default () => (
+  <Layout>
+    <h1>Hello world!</h1>
+    <Link to="/post/">Post Page</Link>
+  </Layout>
+)
